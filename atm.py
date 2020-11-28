510b2c97ed312ad01b892438150a869507bdb7e4
Tobi Oyekanmi <tobititus01@gmail.com>
2020-11-28 18:02:43 +0100

    mysql and datetime modules imported

---
+++ atm.py
@@ -0,0 +1,4 @@
+# this line is be use to import mysql connector with an alias of con in other to link up this whole program to the data base.
+import mysql.connector as con
+# this line imports the date and time module to this program with an alias of dt.
+import datetime as dt
